dd87aeeded81432c9e1aedc859754bc4800b18e0
An <an.le01@sjsu.edu>
2021-10-14 21:54:55 -0700

    Update timing.py

--- timing.py
+++ timing.py
@@ -2,16 +2,16 @@ import time
 
 def calculate_time(func):
     
-    def timer(*args, **kwargs):
-        start_time = time.time()
-        value = func(*args, **kwargs)
-        end_time = time.time()
-        run_time = end_time - start_time
-
-        return value
-    return timer
+	def timer(*args, **kwargs):
+		start_time = time.time()
+		value = func(*args, **kwargs)
+		end_time = time.time()
+		run_time = end_time - start_time
+		print(f'Total time',run_time)
+		return value
+	return timer
 
 @calculate_time
 def func():
-	time.sleep(2)
+		time.sleep(2)
 func()
