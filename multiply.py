a7b084c5c24f5aa0515ff927064f5e78e3a193cc
An <an.le01@sjsu.edu>
2021-10-14 20:17:39 -0700

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,12 +1,4 @@
-'''
-Python program to take in a list as input and multiply all of the 
-elements in the list and return the result as output.
-Parameter
-    inputList: list[]
-                the integer list as input
-    result: int
-                the result of multiplying all of the element
-'''
+
 def multiply_list(inputList):
 
     result = 1; 
@@ -21,7 +13,6 @@ def multiply_list(inputList):
             
     return result
 
-# driver code
 inputList = input("Input: ").split()
 print("Input:[" + ", ".join(inputList) + "]")
 print("Output: ", multiply_list(inputList))
